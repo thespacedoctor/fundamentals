f814930bea04425abe1a1b5a5f6b895c4e620ce3
Dave <d.r.young@qub.ac.uk>
2020-03-10 12:01:04 +0000

    x

--- fundamentals/utKit.py
+++ fundamentals/utKit.py
@@ -136,6 +136,46 @@ class utKit(object):
 
         return None
 
+    def get_project_root(self):
+        """
+        *Get the root of the `python` package - useful for getting files in the root directory of a project*
+
+        **Return:**
+            - ``rootPath`` -- the root path of a project
+        """
+        import os
+        rootPath = os.path.dirname(__file__)
+
+        return rootPath
+
+    def refresh_database(self):
+        """
+        *Refresh the unit test database*
+        """
+        from fundamentals.mysql import directory_script_runner
+        from fundamentals import tools
+        packageDirectory = self.get_project_root()
+        su = tools(
+            arguments={"settingsFile": packageDirectory +
+                       "/test_settings.yaml"},
+            docString=__doc__,
+            logLevel="DEBUG",
+            options_first=False,
+            projectName=None,
+            defaultSettingsFile=False
+        )
+        arguments, settings, log, dbConn = su.setup()
+        directory_script_runner(
+            log=log,
+            pathToScriptDirectory=packageDirectory + "/tests/input",
+            databaseName=settings["database settings"]["db"],
+            force=True,
+            loginPath=settings["database settings"]["loginPath"],
+            waitForResult=True,
+            successRule=None,
+            failureRule=None
+        )
+
 
 if __name__ == '__main__':
     main()
