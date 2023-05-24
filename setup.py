edc8ca7dbf2fd01b5698c62038939da9f3eff636
Dave <d.r.young@qub.ac.uk>
2023-05-24 11:42:50 +0100

    x

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(name="fundamentals",
       classifiers=[
           'Development Status :: 4 - Beta',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.9',
           'Topic :: Utilities',
       ],
       keywords=['logging, database'],
