f5525b8d3a67b0ac769076c353b2a192633a797e
Dave <d.r.young@qub.ac.uk>
2023-09-12 16:39:23 +0100

    x

--- fundamentals/mysql/insert_list_of_dictionaries_into_database_tables.py
+++ fundamentals/mysql/insert_list_of_dictionaries_into_database_tables.py
@@ -100,25 +100,25 @@ def insert_list_of_dictionaries_into_database_tables(
 
     if len(dictList):
         # FIND BUG IN MYSQL QUERY BY UNCOMMENTING
-        tot = len(dictList)
-        for index, d in enumerate(dictList):
-            if index > 1:
-                # Cursor up one line and clear line
-                sys.stdout.write("\x1b[1A\x1b[2K")
+        # tot = len(dictList)
+        # for index, d in enumerate(dictList):
+        #     if index > 1:
+        #         # Cursor up one line and clear line
+        #         sys.stdout.write("\x1b[1A\x1b[2K")
 
-            percent = (float(index) / float(tot)) * 100.
-            print('%(index)s/%(tot)s (%(percent)1.1f%% done)' % locals())
+        #     percent = (float(index) / float(tot)) * 100.
+        #     print('%(index)s/%(tot)s (%(percent)1.1f%% done)' % locals())
 
-            convert_dictionary_to_mysql_table(
-                dbConn=dbConn,
-                log=log,
-                dictionary=d,
-                dbTableName=dbTableName,
-                uniqueKeyList=uniqueKeyList,
-                dateModified=dateModified,
-                reDatetime=reDate,
-                replace=replace,
-                dateCreated=dateCreated)
+        #     convert_dictionary_to_mysql_table(
+        #         dbConn=dbConn,
+        #         log=log,
+        #         dictionary=d,
+        #         dbTableName=dbTableName,
+        #         uniqueKeyList=uniqueKeyList,
+        #         dateModified=dateModified,
+        #         reDatetime=reDate,
+        #         replace=replace,
+        #         dateCreated=dateCreated)
 
         convert_dictionary_to_mysql_table(
             dbConn=dbConn,
