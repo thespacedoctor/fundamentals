37a35f5356c983c3920c814c60cc398e0e6dab57
Dave <d.r.young@qub.ac.uk>
2025-09-24 10:59:15 +0100

    x

--- fundamentals/mysql/insert_list_of_dictionaries_into_database_tables.py
+++ fundamentals/mysql/insert_list_of_dictionaries_into_database_tables.py
@@ -424,9 +424,9 @@ def _add_dictlist_to_database_via_load_in_file(
     csvColumnsString = csvColumnsString.replace(u'`dec`', u'`decl`')
 
     df = pd.DataFrame(dictList)
-    df.replace(['nan', 'None', '', 'NaN', np.nan, None], "\N", inplace=True)
+    df.replace(['nan', 'None', '', 'NaN', np.nan, None], "\\N", inplace=True)
     df.to_csv('/tmp/%(tmpTable)s' % locals(), sep="|",
-              index=False, escapechar="\\", quotechar='"', columns=csvColumns, encoding='utf-8')
+              index=False, escapechar="\\\\", quotechar='"', columns=csvColumns, encoding='utf-8')
 
     sqlQuery = """LOAD DATA LOCAL INFILE '/tmp/%(tmpTable)s'
 INTO TABLE %(tmpTable)s
